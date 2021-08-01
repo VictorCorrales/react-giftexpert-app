87eff1642221f8d4e0ec667813b5ce5f2c89c23c
Victor Manuel <corralesvictor237@gmail.com>
2021-07-31 19:32:04 -0600

    First Commit

---
+++ src/GiftExpertApp.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+
+export const GiftExpertApp = () => {
+
+    const [categories, setCategories] = useState(['Dragon Ball']);
+
+    // const handleAdd = () =>{
+    //     setCategories([...categories,'Jujutsu Kaisen']);
+    // }
+
+    return (
+        <>
+            <h2>GiftExpertApp</h2>
+            <AddCategory setCategories={setCategories} />
+            <hr />
+
+            <ol>
+                {
+                    categories.map(category => (
+                        <GifGrid
+                            key = {category} 
+                            category={category}
+                        />
+                    ))
+                }
+            </ol>
+        </>
+    );
+
+}
